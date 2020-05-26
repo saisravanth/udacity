28c40d5dc68baea3b167215530c1fc1c6d9a8d37
saisravanth <sravgates@gmail.com>
2020-05-26 19:45:43 -0400

    basic flask app

---
+++ Capstone_Projects/CoffeeShop/BasicFlaskAuth/app.py
@@ -0,0 +1,138 @@
+from flask import Flask, request, abort
+import json
+from functools import wraps
+import jwt
+from urllib.request import urlopen
+
+
+app = Flask(__name__)
+
+AUTH0_DOMAIN = 'fsndsai.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'image'
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    print("looks ok !!")
+    token = parts[1]
+    return token
+
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    print("jsonURL: ", jsonurl)
+    jwks = json.loads(jsonurl.read())
+    print("jwks:", jwks)
+
+    unverified_header = jwt.get_unverified_header(token)
+    print("unverified_header:", unverified_header)
+    rsa_key = {}
+
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        print("rsa key:")
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+            print("reached here !!")
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = get_token_auth_header()
+        print("token: ", token)
+        #try:
+        payload = verify_decode_jwt(token)
+        #except:
+        #    abort(401)
+        return f(payload, *args, **kwargs)
+
+    return wrapper
+ 
+@app.route('/headers')
+@requires_auth
+def headers(payload):
+    print(payload)
+    return 'Access Granted'
+
+@app.route('/images')
+@requires_auth
+def images(payload):
+    print(payload)
+    return 'Access Granted'
